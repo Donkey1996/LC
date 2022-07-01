650a79ae4b40073ea11e0ba70cd5ba4c5fc13b1b
Donkey1996 <jerrymengxiao@gmail.com>
2022-07-01 14:41:59 -0500

    1710. Maximum Units on a Truck

---
+++ 07-10-2022/maximum_units_on_a_truck.py
@@ -0,0 +1,55 @@
+from typing import List
+
+
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        sorted_boxes = sorted(boxTypes, key=lambda x: [x[1], x[0]], reverse=False)
+        # sorted boxType ascending
+        result = 0
+        while truckSize > 0:
+            try:
+                box_number, box_size = sorted_boxes.pop()
+            except:
+                break
+            # pop the rightmost element which is the largest in box size
+            if box_number > truckSize:
+                result += truckSize * box_size
+                break
+            else:
+                truckSize -= box_number
+                result += box_number * box_size
+        return result
+
+
+def main():
+    solver = Solution()
+    tests = [
+        {"boxTypes": [[1, 3], [2, 2], [3, 1]], "truckSize": 4, "result": 8},
+        {"boxTypes": [[5, 10], [2, 5], [4, 7], [3, 9]], "truckSize": 10, "result": 91},
+        {
+            "boxTypes": [
+                [1, 3],
+                [5, 5],
+                [2, 5],
+                [4, 2],
+                [4, 1],
+                [3, 1],
+                [2, 2],
+                [1, 3],
+                [2, 5],
+                [3, 2],
+            ],
+            "truckSize": 35,
+            "result": 76,
+        },
+    ]
+    i = 0
+    for test in tests:
+        i += 1
+        output = solver.maximumUnits(test["boxTypes"], test["truckSize"])
+        assert test["result"] == output
+        print("Passed Test #{}!".format(i))
+
+
+if __name__ == "__main__":
+    main()
