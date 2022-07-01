77f935631ce3d09d4da944a94a68c5c4cef6b8dd
Donkey1996 <jerrymengxiao@gmail.com>
2022-07-01 14:51:56 -0500

    1710. Maximum Units on a Truck new

--- 07-10-2022/maximum_units_on_a_truck.py
+++ 07-10-2022/maximum_units_on_a_truck.py
@@ -3,12 +3,16 @@ from typing import List
 
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        sorted_boxes = sorted(boxTypes, key=lambda x: [x[1], x[0]], reverse=False)
+        # sorted_boxes = sorted(boxTypes, key=lambda x: [x[1], x[0]], reverse=False)
+        # sorted_boxes = sorted(boxTypes, key=lambda x: x[1], reverse=False)
+        boxTypes.sort(key=lambda x:x[1], reverse=False)
+        #using boxTypes.sort(key=..) turned out to be slightly faster than storing sorted(), and it uses less memories
+
         # sorted boxType ascending
         result = 0
         while truckSize > 0:
             try:
-                box_number, box_size = sorted_boxes.pop()
+                box_number, box_size = boxTypes.pop()
             except:
                 break
             # pop the rightmost element which is the largest in box size
