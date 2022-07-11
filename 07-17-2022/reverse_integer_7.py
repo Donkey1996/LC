f6ff1c2dfcc27c72493b8987422e02a4243618c8
Donkey1996 <jerrymengxiao@gmail.com>
2022-07-11 14:23:41 -0500

    [7] reverse integer

---
+++ 07-17-2022/reverse_integer_7.py
@@ -0,0 +1,27 @@
+"""
+7. Reverse Integer
+
+"""
+
+from re import M
+from sympy import solve
+
+
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = [1,-1][x < 0]
+        rst = sign * int(str(abs(x))[::-1])
+        return rst if -(2**31)-1 < rst < 2**31 else 0
+
+
+def main():
+    solver = Solution()
+
+    print(solver.reverse(8848))
+
+if __name__ == "__main__":
+    main()
