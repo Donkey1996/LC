0478a030ebb069b4782fe294d5800cd19943f7e4
Donkey1996 <jerrymengxiao@gmail.com>
2022-07-22 12:29:46 -0500

    format

--- binary_tree/same_tree_100.py
+++ binary_tree/same_tree_100.py
@@ -5,7 +5,6 @@ from typing import Optional
 sys.path.insert(0, "/home/jerrymengxiao/dev/LC")
 
 from utils.TreeNode import TreeNode, list_to_tree
-
 """
 1302. Deepest Leaves Sum
 Medium
@@ -14,45 +13,44 @@ Given the root of a binary tree, return the sum of values of its deepest leaves.
 
 """
 
-
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         return self.traversal(p) == self.traversal(q)
-
     def traversal(self, root):
         res = []
         self.search(root, res)
         return res
-
+    
     def search(self, root, res):
         if root:
             res.append(root.val)
             self.search(root.left, res)
-
             self.search(root.right, res)
         else:
-            res.append("Null")
+            res.append('Null')
 
     # def postorder_traversal(self, root):
     #     res = []
     #     self.postorder_search(root, res)
     #     return res
-
+    
     # def postorder_search(self, root, res):
     #     if root:
     #         self.postorder_search(root.left, res)
     #         self.postorder_search(root.right, res)
     #         res.append(root.val)
-
-
 def main():
     tests = [
         {
-            "p": [1, 2, 3, 4, 5, None, 6, 7, None, None, None, None, None, None, 8],
-            "q": [1, 2, 3, 4, 5, None, 6, 7, None, None, None, None, None, None, 8],
-            "result": True,
+            "p": [1,2,3,4,5,None,6,7,None,None,None,None,None,None,8], 
+            "q": [1,2,3,4,5,None,6,7,None,None,None,None,None,None,8],
+            "result": True
         },
-        {"p": [1, 1], "q": [1, None, 1], "result": False},
+        {
+            "p": [1,1], 
+            "q": [1,None,1],
+            "result": False
+        },
     ]
 
     i = 0
