fe93e77c2cfc1bc3abbc322f9f43ba3d85b6a0c7
Donkey1996 <jerrymengxiao@gmail.com>
2022-07-31 22:20:47 -0500

    968

---
+++ utils/serialize.py
@@ -0,0 +1,20 @@
+from TreeNode import TreeNode
+
+
+class Serializer:
+    def __init__():
+        pass
+
+    def serialize(self, root: TreeNode) -> str:
+        pass
+
+    def deserialize(self, data: str) -> TreeNode:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
