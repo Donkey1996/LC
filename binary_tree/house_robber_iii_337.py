46b54b2ddc4ff76159b7befc853cec7295a73637
Donkey1996 <jerrymengxiao@gmail.com>
2022-08-08 12:08:26 -0500

    337 house robber

--- binary_tree/house_robber_iii_337.py
+++ binary_tree/house_robber_iii_337.py
@@ -7,38 +7,46 @@ sys.path.insert(0, "/home/jerrymengxiao/dev/LC")
 from utils.TreeNode import TreeNode, list_to_tree
 
 """
-104. Maximum Depth of Binary Tree
-Easy
+337. House Robber III
+Medium
 
-Given the root of a binary tree, return its maximum depth.
+The thief has found himself a new place for his thievery again. 
+There is only one entrance to this area, called root.
 
-A binary tree's maximum depth is the number of nodes along the longest path 
-from the root node down to the farthest leaf node.
+Besides the root, each house has one and only one parent house. 
+After a tour, the smart thief realized that all houses in this place form a binary tree. 
+It will automatically contact the police if two directly-linked houses were broken into 
+on the same night.
+
+Given the root of the binary tree, return the maximum amount of money the thief 
+can rob without alerting the police.
 """
 
 
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-        visited = defaultdict(list)
-        q = deque([(root, 1)])
-        while q:
-            node, depth = q.popleft()
-            visited[depth].extend([node])
-            if node.left:
-                q.append((node.left, depth + 1))
-            if node.right:
-                q.append((node.right, depth + 1))
+    """
+    https://leetcode.com/problems/house-robber-iii/discuss/376297/Python3-Dynamic-Programming-%2B-Depth-First-Search
+    """
 
-        return sorted(visited)[-1]
+    def rob(self, root: Optional[TreeNode]) -> int:
+        return max(self.dfs(root))
+
+    def dfs(self, root):
+        # state1: rob this root
+        # state2: not rob this root
+        if root is None:
+            return 0, 0
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        sum1 = root.val + left[1] + right[1]
+        sum2 = max(left[0], left[1]) + max(right[0], right[1])
+        return sum1, sum2
 
 
 def main():
     tests = [
-        {"vals": [3, 9, 20, None, None, 15, 7], "result": 3,},
-        {"vals": [1, None, 2], "result": 2},
-        {"vals": [], "result": 0},
+        {"vals": [3, 2, 3, None, 3, None, 1], "result": 7,},
+        {"vals": [3, 4, 5, 1, 3, None, 1], "result": 9},
     ]
 
     i = 0
@@ -47,8 +55,8 @@ def main():
         vals = test["vals"]
         root = list_to_tree(vals)
         solver = Solution()
-        print(solver.maxDepth(root))
-        assert solver.maxDepth(root) == test["result"]
+        print(solver.rob(root))
+        assert solver.rob(root) == test["result"]
         print(f"Passed test case {i}")
 
 
